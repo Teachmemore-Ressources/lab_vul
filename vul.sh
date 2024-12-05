da727e33d18fe0d9c014d7c8116429172aa89175
TEACHMEMORE <150330375+Teachmemore-Ressources@users.noreply.github.com>
2024-12-06 00:02:23 +0100

    Update vul.sh

--- vul.sh
+++ vul.sh
@@ -57,11 +57,7 @@ fi
 
 # Installer Docker Compose
 echo "Installation de Docker Compose..."
-sudo apt install -y curl
-DOCKER_COMPOSE_VERSION=$(curl -s https://api.github.com/repos/docker/compose/releases/latest | grep tag_name | cut -d '"' -f 4)
-sudo curl -L "https://github.com/docker/compose/releases/download/${DOCKER_COMPOSE_VERSION}/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-sudo chmod +x /usr/local/bin/docker-compose
-
+sudo apt install docker-compose -y
 # Vérifier l'installation de Docker Compose
 echo "Vérification de l'installation de Docker Compose..."
 docker-compose --version
